9c703e88a154ae2e432dadca7ef28d371fa4ec21
ThauEx <thau.exodus@gmail.com>
2014-11-05 11:52:38 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+node-image-proxy
+===============
+
+Simple script which will fetch images and caches them.
+
+####Setup
+```
+npm install
+```
+
+####How to start
+```
+nohup node index.js &
+```
+It listens to port 9091
+
+####TODO
+* configurable port
+* optimize images
---
+++ index.js
@@ -0,0 +1,91 @@
+var http = require('needle')
+var express = require('express')
+var app = express()
+var crypto = require('crypto');
+var fs = require('fs');
+
+app.use('/', function (req, res) {
+    var URL = req.url;
+
+    if (URL.substr(0, 5) == '/0x0/') {
+        URL = URL.replace('/0x0/', '');
+    }
+
+    getFile(URL, function(err, data) {
+/*            res.writeHead(200, {
+                'Content-Type': 'image/png'
+            });
+*/
+//            res.write(data);
+            res.end(data, 'binary');
+    });
+})
+
+function getFile(url, callback) {
+    var cacheDir = __dirname + "/cache/";
+    var md5sum = crypto.createHash('md5');
+    var savePath = cacheDir + md5sum.update(url).digest('hex');
+
+    fs.exists(savePath, function(exists) {
+        if (exists) {
+            fileFetch();
+        } else {
+            fetch();
+        }
+    });
+
+    function fileFetch() {
+        fs.readFile(savePath, function(err, imagedata) {
+            if (err) {
+                throw err
+            };
+            callback(err, imagedata);
+
+            fs.stat(savePath, function(err, stats) {
+                var date = new Date();
+                date.setMonth(date.getDay() - 1);
+
+                if (stats.mtime < date) {
+                    fs.unlink(savePath, function() {
+                        console.log("removed old image: " + savePath);
+                    })
+                }
+            })
+        });
+    }
+
+    function fetch() {
+        var out = fs.createWriteStream(savePath);
+        http.get(url).pipe(out);
+        fileFetch();
+        /*http.get(url, function(error, res) {
+            if (!error && res.statusCode < 400) {
+                var imagedata = '';
+                res.setEncoding('binary');
+
+                res.on('readable', function(chunk) {
+                    imagedata += chunk;
+                });
+
+                res.on('end', function() {
+                    writeImage(imagedata);
+                });
+            }
+        });*/
+    }
+
+    function writeImage(imagedata) {
+        fs.writeFile(savePath, imagedata, 'binary', function(err) {
+            if (err) {
+                throw err
+            };
+            fileFetch();
+        });
+    }
+}
+var server = app.listen(9091, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('app listening at http://%s:%s', host, port)
+});
