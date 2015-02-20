81419103c243b1b404ab66d44cd9cac60da03c24
ThauEx <thau.exodus@gmail.com>
2015-02-20 13:16:43 +0100

    code improvements and bug fixing (broken images)

--- index.js
+++ index.js
@@ -1,62 +1,88 @@
-var http = require('needle')
-var express = require('express')
-var app = express()
+'use strict';
+
+var http = require('request');
+var express = require('express');
+var app = express();
 var crypto = require('crypto');
 var fs = require('fs');
 
-app.use('/', function (req, res) {
-    var URL = req.url.substr(1);
-
-    getFile(URL, function(err, data) {
-        res.end(data, 'binary');
-    });
-})
-
 function getFile(url, callback) {
-    var cacheDir = __dirname + "/cache/";
-    var md5sum = crypto.createHash('md5');
-    var savePath = cacheDir + md5sum.update(url).digest('hex');
+    var cacheDir = __dirname + "/cache/",
+        md5sum = crypto.createHash('md5'),
+        savePath = cacheDir + md5sum.update(url).digest('hex'),
+        statusCode,
+        headers;
 
-    fs.exists(savePath, function(exists) {
-        if (exists) {
-            fileFetch();
-        } else {
-            fetch();
-        }
-    });
+    function readFile() {
+        fs.readFile(savePath, function (error, imagedata) {
+            if (error) {
+                throw error;
+            }
 
-    function fileFetch() {
-        fs.readFile(savePath, function(err, imagedata) {
-            if (err) {
-                throw err
-            };
-            callback(err, imagedata);
+            statusCode = 200;
+            headers = {};
 
-            fs.stat(savePath, function(err, stats) {
+            callback(error, imagedata, statusCode, headers);
+
+            fs.stat(savePath, function (error, stats) {
+                if (error) {
+                    throw error;
+                }
+
                 var date = new Date();
-                date.setMonth(date.getDay() - 1);
+                date.setMonth(date.getMonth(), date.getDay() - 1);
 
                 if (stats.mtime < date) {
-                    fs.unlink(savePath, function() {
+                    fs.unlink(savePath, function () {
                         console.log("removed old image: " + savePath);
-                    })
+                    });
                 }
-            })
+            });
         });
     }
 
-    function fetch() {
-        var out = fs.createWriteStream(savePath);
-        http.get(url, function(error, response) {
-            fileFetch();
-        }).pipe(out);
+    function downloadFile() {
+        var fileStream = fs.createWriteStream(savePath);
+
+        fileStream.on('close', function () {
+            readFile();
+        });
+
+        http(url)
+            .on('response', function (response) {
+                statusCode = response.statusCode;
+                headers = response.headers;
+            })
+            .pipe(fileStream);
     }
+
+    fs.exists(savePath, function (exists) {
+        if (exists) {
+            readFile();
+        } else {
+            downloadFile();
+        }
+    });
 }
 
 app.set('port', process.env.PORT || 9091);
 var server = app.listen(app.get('port'), function () {
-    var host = server.address().address
-    var port = server.address().port
+    var host = server.address().address,
+        port = server.address().port;
 
-    console.log('app listening at http://%s:%s', host, port)
+    console.log('app listening at http://%s:%s', host, port);
+});
+
+
+app.use('/', function (req, res) {
+    var URL = req.url.substr(1);
+
+    getFile(URL, function (error, data, statusCode, headers) {
+        if (error) {
+            throw error;
+        }
+
+        res.writeHead(statusCode, headers);
+        res.end(data, 'binary');
+    });
 });
