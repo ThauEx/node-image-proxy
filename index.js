b7f42f9291d4922b5a587d216a101c949736c74c
ThauEx <thau.exodus@gmail.com>
2015-02-24 08:13:47 +0100

    fixed #2 and added dummy image on failures

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ var express = require('express');
 var app = express();
 var crypto = require('crypto');
 var fs = require('fs');
+var imageType = require('image-type');
 
 function getFile(url, callback) {
     var cacheDir = __dirname + "/cache/",
         md5sum = crypto.createHash('md5'),
         savePath = cacheDir + md5sum.update(url).digest('hex'),
         statusCode,
-        headers;
+        headers,
+        invalidImage = false;
 
     function readFile() {
         fs.readFile(savePath, function (error, imagedata) {
@@ -41,28 +43,66 @@ function getFile(url, callback) {
         });
     }
 
-    function downloadFile() {
-        var fileStream = fs.createWriteStream(savePath);
+    function showDummyImage() {
+        statusCode = 400;
+        headers = {};
 
-        fileStream.on('close', function () {
-            readFile();
+        fs.unlink(savePath, function () {
+            console.log("removed invalid image: " + savePath);
         });
 
-        http(url)
+        callback(
+            '',
+            new Buffer('R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==', 'base64'),
+            statusCode,
+            headers
+        );
+    }
+
+    function downloadFile() {
+        var fileStream = fs.createWriteStream(savePath),
+            request = http(url,
+                function (error) {
+                    if (error) {
+                        invalidImage = true;
+                        console.log(error);
+                        showDummyImage();
+                    }
+                });
+
+        request
+            .on('data', function (data) {
+                if (!imageType(data)) {
+                    invalidImage = true;
+                    console.log('Url is no image: ' + url);
+                    request.abort();
+                    showDummyImage();
+                }
+            })
             .on('response', function (response) {
                 statusCode = response.statusCode;
                 headers = response.headers;
             })
             .pipe(fileStream);
+
+        fileStream.on('close', function () {
+            if (!invalidImage) {
+                readFile();
+            }
+        });
     }
 
-    fs.exists(savePath, function (exists) {
-        if (exists) {
-            readFile();
-        } else {
-            downloadFile();
-        }
-    });
+    if (url) {
+        fs.exists(savePath, function (exists) {
+            if (exists) {
+                readFile();
+            } else {
+                downloadFile();
+            }
+        });
+    } else {
+        showDummyImage();
+    }
 }
 
 app.set('port', process.env.PORT || 9091);
